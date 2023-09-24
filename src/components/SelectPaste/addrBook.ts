dfe29c01947e62182d900a795cfd42bf7b386d2c
redwest88 <71982739+redwest88@users.noreply.github.com>
2023-09-24 13:58:10 +0200

    addrBook.ts updated

--- src/components/SelectPaste/addrBook.ts
+++ src/components/SelectPaste/addrBook.ts
@@ -1,3 +1,3 @@
-export const addrBook = `curl -s {{endpoint}} > $HOME/.zetacored/config/addrbook.json
+export const addrBook = `curl -s {{endpoint}} > $HOME/.{{workingDirectory}}/config/addrbook.json
 
-sudo systemctl restart zetacored`;
+sudo systemctl restart {{binary}}`;
