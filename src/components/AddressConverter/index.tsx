e9f37eca6a2bd587cf2610bd9b606adaa594e702
Influenzo <52459025+vasylcode@users.noreply.github.com>
2023-10-27 17:58:20 -0600

    package upd

--- src/components/AddressConverter/index.tsx
+++ src/components/AddressConverter/index.tsx
@@ -1,59 +1,59 @@
-import React, { useState } from "react";
-import { bech32 } from "bech32";
+// import React, { useState } from "react";
+// import { bech32 } from "bech32";
 
-export const AddressConverter: React.FC<{}> = () => {
-	const [address, setAddress] = useState("");
-	const [convertedAddress, setConvertedAddress] = useState("");
-	const [hrp, setHrp] = useState("");
+// export const AddressConverter: React.FC<{}> = () => {
+// 	const [address, setAddress] = useState("");
+// 	const [convertedAddress, setConvertedAddress] = useState("");
+// 	const [hrp, setHrp] = useState("");
 
-	const handleAddressChange = (e: any) => {
-		setAddress(e.target.value);
-	};
+// 	const handleAddressChange = (e: any) => {
+// 		setAddress(e.target.value);
+// 	};
 
-	const handleHrpChange = (e: any) => {
-		setHrp(e.target.value);
-	};
+// 	const handleHrpChange = (e: any) => {
+// 		setHrp(e.target.value);
+// 	};
 
-	const handleConvert = () => {
-		try {
-			let converted;
-			if (address.startsWith("0x")) {
-				const data = Buffer.from(address.substr(2), "hex");
-				converted = bech32.encode(hrp, bech32.toWords(data));
-			} else {
-				const decoded = bech32.decode(address);
-				converted = "0x" + Buffer.from(bech32.fromWords(decoded.words)).toString("hex");
-			}
+// 	const handleConvert = () => {
+// 		try {
+// 			let converted;
+// 			if (address.startsWith("0x")) {
+// 				const data = Buffer.from(address.substr(2), "hex");
+// 				converted = bech32.encode(hrp, bech32.toWords(data));
+// 			} else {
+// 				const decoded = bech32.decode(address);
+// 				converted = "0x" + Buffer.from(bech32.fromWords(decoded.words)).toString("hex");
+// 			}
 
-			setConvertedAddress(converted);
-		} catch (error) {}
-	};
+// 			setConvertedAddress(converted);
+// 		} catch (error) {}
+// 	};
 
-	return (
-		<div>
-			<div className="flex items-center gap-3 mb-3">
-				<input
-					type="text"
-					value={hrp}
-					onChange={handleHrpChange}
-					placeholder="HRP (e.g., dydx)"
-					className="bg-yellow-800 dark:bg-red-800"
-				/>
-				<input
-					type="text"
-					value={address}
-					onChange={handleAddressChange}
-					placeholder="0x or Bech32 address"
-					className="bg-yellow-800 dark:bg-red-800"
-				/>
+// 	return (
+// 		<div>
+// 			<div className="flex items-center gap-3 mb-3">
+// 				<input
+// 					type="text"
+// 					value={hrp}
+// 					onChange={handleHrpChange}
+// 					placeholder="HRP (e.g., dydx)"
+// 					className="bg-yellow-800 dark:bg-red-800"
+// 				/>
+// 				<input
+// 					type="text"
+// 					value={address}
+// 					onChange={handleAddressChange}
+// 					placeholder="0x or Bech32 address"
+// 					className="bg-yellow-800 dark:bg-red-800"
+// 				/>
 
-				<button type="button" onClick={handleConvert} className="hover:dark:border-green-500">
-					Convert
-				</button>
-			</div>
-			<p>Converted address: {convertedAddress}</p>
-		</div>
-	);
-};
+// 				<button type="button" onClick={handleConvert} className="hover:dark:border-green-500">
+// 					Convert
+// 				</button>
+// 			</div>
+// 			<p>Converted address: {convertedAddress}</p>
+// 		</div>
+// 	);
+// };
 
-export default AddressConverter;
+// export default AddressConverter;
