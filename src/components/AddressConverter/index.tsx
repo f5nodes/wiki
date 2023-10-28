1ce6a7a957c46f2445c2a4081a2d5e8e9cd06e96
Vasyl <vasylcode@gmail.com>
2023-10-27 18:33:26 -0600

    package & converter fixed

--- src/components/AddressConverter/index.tsx
+++ src/components/AddressConverter/index.tsx
@@ -1,59 +1,74 @@
-// import React, { useState } from "react";
-// import { bech32 } from "bech32";
+import React, { useState } from "react";
+import { bech32 } from "bech32";
 
-// export const AddressConverter: React.FC<{}> = () => {
-// 	const [address, setAddress] = useState("");
-// 	const [convertedAddress, setConvertedAddress] = useState("");
-// 	const [hrp, setHrp] = useState("");
+export const AddressConverter: React.FC<{}> = () => {
+	const [address, setAddress] = useState("");
+	const [convertedAddress, setConvertedAddress] = useState("");
+	const [network, setNetwork] = useState(""); // Default network
 
-// 	const handleAddressChange = (e: any) => {
-// 		setAddress(e.target.value);
-// 	};
+	const handleAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setAddress(e.target.value);
+	};
 
-// 	const handleHrpChange = (e: any) => {
-// 		setHrp(e.target.value);
-// 	};
+	const handleNetworkChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setNetwork(e.target.value);
+	};
 
-// 	const handleConvert = () => {
-// 		try {
-// 			let converted;
-// 			if (address.startsWith("0x")) {
-// 				const data = Buffer.from(address.substr(2), "hex");
-// 				converted = bech32.encode(hrp, bech32.toWords(data));
-// 			} else {
-// 				const decoded = bech32.decode(address);
-// 				converted = "0x" + Buffer.from(bech32.fromWords(decoded.words)).toString("hex");
-// 			}
+	const handleConvert = () => {
+		try {
+			let converted;
 
-// 			setConvertedAddress(converted);
-// 		} catch (error) {}
-// 	};
+			if (address.startsWith("0x")) {
+				// Convert from hexadecimal to Bech32
+				const hexData = address.substr(2);
+				const bytes = new Uint8Array(hexData.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+				const words = bech32.toWords(bytes);
+				converted = bech32.encode(network, words);
+			} else if (address.includes("1")) {
+				// Convert from Bech32 to hexadecimal
+				const decoded = bech32.decode(address);
+				const bytes = new Uint8Array(bech32.fromWords(decoded.words));
+				converted =
+					"0x" +
+					Array.from(bytes)
+						.map((byte) => byte.toString(16).padStart(2, "0"))
+						.join("");
+			} else {
+				// Handle invalid input address format
+				throw new Error("Invalid address format");
+			}
 
-// 	return (
-// 		<div>
-// 			<div className="flex items-center gap-3 mb-3">
-// 				<input
-// 					type="text"
-// 					value={hrp}
-// 					onChange={handleHrpChange}
-// 					placeholder="HRP (e.g., dydx)"
-// 					className="bg-yellow-800 dark:bg-red-800"
-// 				/>
-// 				<input
-// 					type="text"
-// 					value={address}
-// 					onChange={handleAddressChange}
-// 					placeholder="0x or Bech32 address"
-// 					className="bg-yellow-800 dark:bg-red-800"
-// 				/>
+			setConvertedAddress(converted);
+		} catch (error) {
+			setConvertedAddress("Error: Invalid address or network");
+		}
+	};
 
-// 				<button type="button" onClick={handleConvert} className="hover:dark:border-green-500">
-// 					Convert
-// 				</button>
-// 			</div>
-// 			<p>Converted address: {convertedAddress}</p>
-// 		</div>
-// 	);
-// };
+	return (
+		<div>
+			<div className="flex items-center gap-3 mb-3">
+				<input
+					type="text"
+					value={network}
+					onChange={handleNetworkChange}
+					placeholder="Network (e.g., dydx)"
+					className="bg-yellow-800 dark:bg-red-800"
+				/>
+				<input
+					type="text"
+					value={address}
+					onChange={handleAddressChange}
+					placeholder="0x or Bech32 address"
+					className="bg-yellow-800 dark:bg-red-800"
+				/>
 
-// export default AddressConverter;
+				<button type="button" onClick={handleConvert} className="hover:dark:border-green-500">
+					Convert
+				</button>
+			</div>
+			<p>Converted address: {convertedAddress}</p>
+		</div>
+	);
+};
+
+export default AddressConverter;
